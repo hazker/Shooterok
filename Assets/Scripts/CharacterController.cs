a02626323552261a1da66d432ba4556999bb0b21
hazker <zov.zon@gmail.com>
2019-09-05 23:17:23 +0700

    Start

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class CharacterController : MonoBehaviour {
+
+    [SerializeField]Camera mainCamera;
+	public float mouseSensitivity = 5f;                 //чувствительность мыши
+    public float speed = 5f;                            //скорость персонажа
+    public float runMultiple = 2f;                      //множитель бега
+    public float maxVelocityChange = 10f;               //максимальное изменение скорости
+    public float maxVerticalAngle = 60f;                //максимальное отклонение камеры по горизантали
+    public float sitHeight = 0.75f;                     //высота присеста
+    
+    private Rigidbody rigidbodyBody;
+    private Quaternion originCameraRotation;            //начальные оси поворота камеры
+    private Vector3 originCameraPosition;               //начальные координаты камеры
+    private float angleHorizontal;                      //углы поворота по осям
+    private float angleVertical;
+    private float diagonalMultiple = 1 / Mathf.Sqrt(2); //множитель ходьбы по диагонали
+    private bool isRun = false;
+    private bool isSit = false;
+
+	private void Awake() {
+        rigidbodyBody = GetComponent<Rigidbody>();
+        originCameraRotation = mainCamera.transform.rotation;
+        originCameraPosition = mainCamera.transform.localPosition;
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void FixedUpdate() {
+        angleHorizontal += Input.GetAxis("Mouse X") * mouseSensitivity;
+        angleVertical += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        angleVertical = Mathf.Clamp(angleVertical, -maxVerticalAngle, maxVerticalAngle);
+
+        Quaternion rotationY = Quaternion.AngleAxis(angleHorizontal, Vector3.up);
+        rigidbodyBody.MoveRotation(rotationY);
+        rigidbodyBody.AddForce(GetVelocityChange(), ForceMode.VelocityChange);
+	}
+
+    private Vector3 GetVelocityChange() {
+        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= speed;
+
+        if (targetVelocity.x != 0 && targetVelocity.z != 0) { //движение по диагонали
+            targetVelocity *= diagonalMultiple;
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetAxis("Vertical") > 0 && !isSit) { //бег
+            isRun = true;
+            targetVelocity *= runMultiple;
+        }
+        else {
+            isRun = false;
+        }
+
+        Vector3 velocityChange = (targetVelocity - rigidbodyBody.velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+
+        return velocityChange;
+    }
+
+    private void LateUpdate() {
+        Quaternion rotationY = Quaternion.AngleAxis(angleHorizontal, Vector3.up);
+        Quaternion rotationX = Quaternion.AngleAxis(-angleVertical, Vector3.right);
+
+        mainCamera.transform.rotation = originCameraRotation * rotationY * rotationX;
+
+        if (Input.GetKey(KeyCode.LeftControl) && !isSit && !isRun) { //присест
+            isSit = true;
+            mainCamera.transform.localPosition = originCameraPosition - new Vector3(0, sitHeight, 0);
+        }
+        else if (!Input.GetKey(KeyCode.LeftControl) && isSit) {
+            isSit = false;
+            mainCamera.transform.localPosition = originCameraPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public float power = 50f;
+    public float distance = 100;
+    public Transform firepoint;
+
+    private RaycastHit hit;
+
+    public GameObject shellPref;
+    public Transform  shellspawn;
+    
+
+    private GameObject shell;
+    public float shellpower = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = firepoint.transform.TransformDirection(Vector3.forward) * 100;
+        Debug.DrawRay(firepoint.transform.position, forward, Color.green);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+            shellout();
+        }
+    }
+    private void Shot()
+    {
+        if(Physics.Raycast(firepoint.position, transform.forward, out hit, distance))
+        {
+            Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+            if(rb!=null && !rb.isKinematic && rb.constraints == RigidbodyConstraints.None)
+            {
+                Vector3 direction = (hit.point - transform.position).normalized;
+                rb.AddForceAtPosition(direction * power, hit.point);
+              
+            }
+            Debug.Log("Shoot: " + hit.transform.name);
+        }
+        else
+        {
+            Debug.Log("Miss");
+        }
+        
+    }
+    private void shellout()
+    {
+        shell = Instantiate(shellPref, shellspawn.position, shellspawn.rotation);
+        shell.GetComponent<Rigidbody>().AddForce(shell.transform.right * shellpower, ForceMode.Impulse);
+        Destroy(shell, 2f);
+    }
+}
