ec5c7faec67bf38bcb8f00f96b217d58cc88715d
hazker <zov.zon@gmail.com>
2019-10-06 23:57:37 +0700

    Version 0.00001

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField] Behaviour[] componentsToDisable;
+
+    private Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            camera = Camera.main;
+            if (camera != null)
+            {
+                camera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void OnDisable()
+    {
+        if (camera != null)
+        {
+            camera.gameObject.SetActive(true);
+        }
+    }
+}
